1fbd5a54bc7dec15c7c2bfa401de5e3e0d3f3fe2
dottilorenzo <dottilorenzo575@gmail.com>
2026-04-08 16:08:28 -0300

    Delete startup/startup directory

--- startup/startup/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:8080/colecaoSeries', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB conectado'))
-.catch(() => console.error('Não possível conectar ao MongoDB'));
-
